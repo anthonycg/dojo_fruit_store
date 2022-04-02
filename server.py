e80474fb1f867440a6716b13e6f0c5c822c70da7
Anthony <anthonygibson24@gmail.com>
2022-04-02 01:44:42 -0500

    added data from session to display on sceen

--- server.py
+++ server.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template, request, redirect
+#!/usr/bin/env python3
+
+from flask import Flask, render_template, request, redirect, session
+from datetime import datetime
 app = Flask(__name__)  
 
 @app.route('/')         
@@ -8,7 +11,21 @@ def index():
 @app.route('/checkout', methods=['POST'])         
 def checkout():
     print(request.form)
-    return render_template("checkout.html")
+    client_first_name = request.form['first_name']
+    client_last_name = request.form['last_name']
+    strawberry_total = request.form['strawberry']
+    apple_total = request.form['apple']
+    raspberry_total = request.form['raspberry']
+    fruit_total = int(strawberry_total) + int(apple_total) + int(raspberry_total)
+    student_id = request.form['student_id']
+    current_datetime =datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+
+    # session['apple'] = request.form['apple']
+    # session['strawberry'] = request.form['strawberry']
+    # session['rasberry'] = request.form['rasberry']
+    return render_template("checkout.html", first_name=client_first_name, last_name=client_last_name,
+    fruit_total=fruit_total, strawberry_total = strawberry_total, apple_total=apple_total, raspberry_total=raspberry_total,
+    student_id=student_id, current_datetime=current_datetime)
 
 @app.route('/fruits')         
 def fruits():
